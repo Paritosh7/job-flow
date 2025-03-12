2ae1a62a7e386e56e6893992db00c45e70ad0132
Paritosh7 <paritosh7.sahni@gmail.com>
2025-03-12 17:46:52 +0000

    migration changes

--- jobs/models.py
+++ jobs/models.py
@@ -7,7 +7,7 @@ class Item(models.Model):
     id = models.UUIDField(default=uuid.uuid4,primary_key=True, editable=False)
     item_name = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateField(auto_now=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
 
 class Job(models.Model):
